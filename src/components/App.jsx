e7fa250cf7db7c8d6553db1b609d21ab3c90db1b
RomanZakrenychnyi <116659319+RomanZakrenychnyi@users.noreply.github.com>
2023-03-29 11:55:45 +0300

    update LS

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,19 +7,16 @@ import { Filter } from './Filter/Filter';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
   componentDidMount() {
-    const getContactsLS = localStorage.getItem('contacts');
-    const parsedContactLS = JSON.parse(getContactsLS);
-    this.setState({ contacts: parsedContactLS });
+    const getContactsLS = JSON.parse(localStorage.getItem('contacts'));
+    if (!getContactsLS) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+    }
+    this.setState({ contacts: getContactsLS });
   }
 
   componentDidUpdate(preveProps, prevState) {
