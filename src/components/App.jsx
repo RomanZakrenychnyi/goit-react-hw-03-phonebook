8150719f42c2e96ff27b936c2204d3a0c72a7567
RomanZakrenychnyi <116659319+RomanZakrenychnyi@users.noreply.github.com>
2023-03-29 11:26:18 +0300

    create new repo for hm

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,65 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { nanoid } from 'nanoid';
+import { Component } from 'react';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Section } from './Section/Section';
+import { Filter } from './Filter/Filter';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  handleAddContact = event => {
+    event.preventDefault();
+    const name = event.target[0].value;
+    const number = event.target[1].value;
+    this.setState(prevState => {
+      return {
+        contacts: [...prevState.contacts, { name, number, id: nanoid() }],
+      };
+    });
+  };
+
+  handelFilter = event => {
+    this.setState({ filter: event.target.value });
+  };
+
+  filteredContacts = () => {
+    const { contacts, filter } = this.state;
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter?.toLowerCase())
+    );
+  };
+
+  handleDeleteBtnClick = id => {
+    this.setState(prevState => {
+      return {
+        contacts: prevState.contacts.filter(contact => contact.id !== id),
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <Section title="Phonebook">
+          <ContactForm handleAddContact={this.handleAddContact} />
+        </Section>
+        <Filter handelFilter={this.handelFilter} />
+        <Section title="Contacts">
+          <ContactList
+            contacts={this.filteredContacts()}
+            handleDeleteBtnClick={this.handleDeleteBtnClick}
+          />
+        </Section>
+      </div>
+    );
+  }
+}
